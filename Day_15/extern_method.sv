5553caad2a0a7172f4f3aaf3cc52eeb007402081
Makesh Kumar <makeshkumar1910@gmail.com>
2025-12-09 17:57:17 +0530

    Update extern_method.sv

--- Day_15/extern_method.sv
+++ Day_15/extern_method.sv
@@ -3,17 +3,15 @@ class bus;
   int id;
 
   extern function void display();
-    extern task show(bit [31:0]data = 500,int id = 10);
+    extern task show();
       endclass
 
       function void bus::display();
         $display("Data = %0d & ID = %0d",data,id);
       endfunction
     
-      task bus::show(bit [31:0]data = 500,int id = 10);
-      this.data=data;
-      this.id=id;
-        $display("Data = %0d & ID = %0d",data,id);
+      task bus::show();
+        $display("Bit = %0d & Int = %0d",data,id);
       endtask
 
 module extern_method;
@@ -32,5 +30,5 @@ endmodule
 
 
 # Data = 100 & ID = 1
-# Data = 500 & ID = 10
+# Bit = 100 & Int = 1
 # exit

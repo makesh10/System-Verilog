8994b2d36901cc1f89d5892e2c4a08f79ffa10b3
Makesh Kumar <makeshkumar1910@gmail.com>
2025-12-11 15:17:31 +0530

    Create fork_join_none.sv

---
+++ Day_16/fork_join_none.sv
@@ -0,0 +1,34 @@
+module fork_join_none;
+  
+  initial begin
+    fork
+      begin
+        $display("Process A started at time = %0t",$time);
+        #10;
+        $display("Process A completed at time = %0t",$time);
+      end
+      begin
+        $display("Process B started at time = %0t",$time);
+        #15;
+        $display("Process B completed at time = %0t",$time);
+      end
+      begin
+        $display("Process C started at time = %0t",$time);
+        #20;
+        $display("Process C completed at time = %0t",$time);
+      end
+    join_none
+    $display("fork-join_none completed at time = %0t",$time);
+  end
+  
+endmodule
+
+
+# fork-join_none completed at time = 0
+# Process A started at time = 0
+# Process B started at time = 0
+# Process C started at time = 0
+# Process A completed at time = 10
+# Process B completed at time = 15
+# Process C completed at time = 20
+# exit

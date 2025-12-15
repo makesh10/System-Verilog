79a1e66d38971b5e336fcc3a94c86de372703cb3
Makesh Kumar <makeshkumar1910@gmail.com>
2025-12-15 15:09:51 +0530

    Create event_type3.sv

---
+++ Day_17/event_type3.sv
@@ -0,0 +1,29 @@
+module events;
+  event e;
+  
+  task task_A();
+    $display("@%0t: Before triggering event e",$time);
+    ->e;
+    $display("@%0t: After triggering event e",$time);
+  endtask
+  
+  task task_B();
+    $display("@%0t: waiting for the event e",$time);
+    @e;
+    $display("@%0t: event e is triggered!",$time);
+  endtask
+
+  initial begin
+    fork
+      task_A();
+      task_B();
+    join
+  end
+  
+endmodule
+
+
+# @0: Before triggering event e
+# @0: After triggering event e
+# @0: waiting for the event e
+# exit
